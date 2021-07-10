ed8f9d2beb5ff3d15ebd162dbb152458f1f21523
Ifthaker Ahmed <69840545+project-fish@users.noreply.github.com>
2021-07-10 17:45:49 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react'
 import { Cards, Chart, CountryPicker} from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
+
 import image from './images/img.png';
+
 
 class App extends React.Component{
 
@@ -29,7 +31,7 @@ class App extends React.Component{
 
         return(
             <div className={styles.container}>
-                <img className={styles.img} alt="covid-19"/>
+                <img className={styles.img} src={image} alt="covid-19"/>
                 <Cards data={data}/>
                 <CountryPicker handleCountryChange={this.handleCountryChange} />
                 <Chart data={data} country={country}/>
